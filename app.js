5cb4af8fdea70357963bd9c6b0a3d00bd4d756f7
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-09-10 03:59:45 +0000

    [Update] JS練習 3章

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-let str1 = 'hello';
-let str2 = 'world!!!';
-alert(str1 + str2);
+let alertString;
+alertString = addString("WebCamp");
 
-let orange = 100;
-let apple = 120;
+alert(alertString);
 
-if(orange < apple){
-  alert('みかんの値段がリンゴより安い');
-} else if(orange == apple){
-  alert('みかんとリンゴが同じ値段')
-} else {
-  alert('みかんの値段がリンゴより高い')
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
 }
-
-let i;
-let num = 0;
-
-for(i = 1; i < 11; i++){
-  num = num + i;
-}
-
-alert('1から10まで足し算した後は' + num + 'です');
